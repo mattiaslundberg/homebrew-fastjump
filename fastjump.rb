af40c67cf8abda9454b714ad48504daa4381ecc4
Mattias Lundberg <mattias@kundo.se>
2019-09-08 11:17:54 +0200

    Upgrade to version 0.2.0

--- fastjump.rb
+++ fastjump.rb
@@ -1,15 +1,14 @@
 class Fastjump < Formula
   desc "Tool for quickly navigating between folders"
   homepage "https://github.com/mattiaslundberg/fastjump"
-  url "https://github.com/mattiaslundberg/fastjump/archive/v0.1.0.tar.gz"
-  sha256 "4027ff4b4cc7be48c30b16f7fc0e9db5041a4d4ce69ada6204588a99b5809dc3"
-  version "0.1.0"
+  url "https://github.com/mattiaslundberg/fastjump/archive/v0.2.0.tar.gz"
+  sha256 "913d3ce72e254593299d83c0992d64cf6b1afa3718b0f7e220b1bf032d8f0c80"
+  version "0.2.0"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", "--root", prefix,
-                               "--path", "."
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
