1b364468d69aadddaf524b3f09d95ae18f9219ec
Mattias Lundberg <mattias@kundo.se>
2019-11-19 08:26:23 +0100

    v0.6.1

--- fastjump.rb
+++ fastjump.rb
@@ -1,9 +1,9 @@
 class Fastjump < Formula
   desc "Tool for quickly navigating between folders"
   homepage "https://github.com/mattiaslundberg/fastjump"
-  url "https://github.com/mattiaslundberg/fastjump/archive/0.6.1.tar.gz"
-  sha256 "28e568a314451dabf91012068bd299861f25f15053c45e9735772891ad383ef6"
-  version "0.6.1"
+  url "https://github.com/mattiaslundberg/fastjump/archive/v0.6.1.tar.gz"
+  sha256 "70ff467a60efa36a8287224912491c17e19390eaa543c162174922a9f0729299"
+  version "v0.6.1"
 
   depends_on "rust" => :build
 
